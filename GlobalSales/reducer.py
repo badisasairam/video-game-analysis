e7f4ddc750a7fe91bc20a86f83b4d317970b3954
badisasairam <42946279+badisasairam@users.noreply.github.com>
2019-09-30 19:15:42 -0500

    Update reducer.py

--- GlobalSales/reducer.py
+++ GlobalSales/reducer.py
@@ -15,19 +15,21 @@ oldKey = None
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
     if len(data_mapped) != 2:
-        # Something has gone lwrong. Skip this line.
+        # Something has gone lwrong. Skip this line. <br>
         continue
-
+        # this step assigns the values of the data_mapped to the thisKey, thisSale. <br>
     thisKey, thisSale = data_mapped
-
+    
+        # if oldKey and oldkey not equals to thisKey then it is going to print both values and assign thisKey to oldKey and salesTotal to 0. <br>
     if oldKey and oldKey != thisKey:
         print oldKey, "\t", salesTotal
         oldKey = thisKey
         salesTotal = 0
 
+        # this steps proceeds with the assigning the thisKey to oldKey and float value of thisSale to salesTotal. <br>
     oldKey = thisKey
     salesTotal += float(thisSale)
-
+        # if oldKey is not equal to anything then it is going to print oldKey and salesTotal. <br>
 if oldKey != None:
     print oldKey, "\t", salesTotal
 
